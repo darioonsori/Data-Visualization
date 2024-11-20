0d815a2c678b75d85e3d99b77b6e5677242851e0
darioonsori <73284471+darioonsori@users.noreply.github.com>
2024-11-20 15:57:12 +0100

    Update alluvialPlot.js

--- js/alluvialPlot.js
+++ js/alluvialPlot.js
@@ -1,4 +1,4 @@
-d3.csv("co2-fossil-plus-land-use.csv").then(function(data) {
+d3.csv("data/co2-fossil-plus-land-use/co2-fossil-plus-land-use.csv").then(function(data) {
     console.log(data); // Controlla il contenuto del CSV nella console
 }).catch(function(error) {
     console.error("Error loading the CSV file:", error); // Mostra errori se il file non viene caricato
